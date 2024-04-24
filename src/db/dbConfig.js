635a4b053afa135aef75f3dc59a5cd6f53cf5ecf
Alexander Stanevko <dardawill@yandex.ru>
2024-04-24 15:13:19 +0300

    wip

--- src/db/dbConfig.js
+++ src/db/dbConfig.js
@@ -1,15 +1,10 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-export const sequelize = new Sequelize('url_shortener_db', 'root', '227235', {
-  host: 'localhost',
-  port: 3307,
+dotenv.config();
+
+export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: 'mysql',
 });
-
-// import { Sequelize } from 'sequelize';
-
-// export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   dialect: 'mysql',
-// });
