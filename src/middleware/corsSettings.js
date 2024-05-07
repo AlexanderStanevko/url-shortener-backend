3230fb9c5b7fe6a5212e2fc76fed174a9c384dbe
Alexander Stanevko <dardawill@yandex.ru>
2024-05-07 18:38:54 +0300

    wip

--- src/middleware/corsSettings.js
+++ src/middleware/corsSettings.js
@@ -1,9 +1,8 @@
 import cors from 'cors';
 
 const corsOptions = {
-  origin: 'http://localhost:9000',
+  origin: ['http://localhost:9000', 'https://hammerhead-app-cngdy.ondigitalocean.app'],
   optionsSuccessStatus: 200
 };
 
 export const corsMiddleware = cors(corsOptions);
-
