f3fa93116f9c09beabedaa02b138596120958883
Alexander Stanevko <dardawill@yandex.ru>
2024-05-07 22:03:26 +0300

    wip

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,14 +3,14 @@ import testRoute from './testRoute.js';
 import authRoutes from './authRoutes.js'
 import shortUrlRoutes from './shortUrlRoutes.js'
 import shortUrlRedirectRoutes from './shortUrlRedirectRoutes.js'
-import { authenticateToken } from '../middleware/index.js';
+import { optionalAuthenticateToken } from '../middleware/index.js';
 
 
 const router = express.Router();
 
 router.use('/test', testRoute);
 router.use('/auth', authRoutes);
-router.use('/short', authenticateToken, shortUrlRoutes);
+router.use('/short', optionalAuthenticateToken, shortUrlRoutes);
 router.use('/', shortUrlRedirectRoutes);
 
 export default router;
